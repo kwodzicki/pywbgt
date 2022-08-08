da07474126b979581d50925a2bc7756af3016ae4
Kyle R. Wodzicki <kwodzicki@cicsnc.org>
2022-08-08 11:07:26 -0400

    Validations plots all 4 panel now
    The validation plotting code has been updated so that each
    function generates a 4-panel plot containing comparisons between
    globe temperature, psychrometric wet bulb, natural wet bulb, and
    wet bulb globe temperature.

--- seus_hvi_wbgt/validation/__init__.py
+++ seus_hvi_wbgt/validation/__init__.py
@@ -4,11 +4,11 @@ from seus_hvi_wbgt.validation import wbgt_by_solar_method
 from seus_hvi_wbgt.validation import wbgt_by_rh_method
 
 
-def main():
+def main( **kwargs ):
   data, meta = wbgt_by_wind_method.loadData()
 
-  xx = wbgt_by_wind_method.main(data, meta)
-  xx = wbgt_by_station_method.main(data, meta)
-  xx = wbgt_by_solar_method.main(data, meta)
-  xx = wbgt_by_rh_method.main(data, meta)
+  xx = wbgt_by_wind_method.main(    data, meta, **kwargs )
+  xx = wbgt_by_station_method.main( data, meta, **kwargs )
+  xx = wbgt_by_solar_method.main(   data, meta, **kwargs )
+  xx = wbgt_by_rh_method.main(      data, meta, **kwargs )
   
