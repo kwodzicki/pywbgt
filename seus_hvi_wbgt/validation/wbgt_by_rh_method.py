a345a37e4c9b4369bbfb5ac447d5df25c844c32c
Kyle R. Wodzicki <kwodzicki@cicsnc.org>
2022-08-08 11:07:26 -0400

    Validations plots all 4 panel now
    The validation plotting code has been updated so that each
    function generates a 4-panel plot containing comparisons between
    globe temperature, psychrometric wet bulb, natural wet bulb, and
    wet bulb globe temperature.

--- seus_hvi_wbgt/validation/wbgt_by_rh_method.py
+++ seus_hvi_wbgt/validation/wbgt_by_rh_method.py
@@ -19,21 +19,24 @@ from .utils import *
 matplotlib.rcParams.update({'font.size': 8})
 ALPHA = 0.5
 
-def main( *args ):
+def main( *args, methods = None, nCol = 7, **kwargs):
   if len( args ) != 2:
     data, meta = c.download()
   else:
     data, meta = args
 
+  if methods is None:
+    methods     = ['liljegren', 'dimiceli', 'bernard']                            # Set names for different WBGT methods
+
+  if len(methods) > 3:
+    raise Exception( 'Can only test three (3) methods at a time!' )
+
   markers     = ['o', 'v', '^']                                                 # Set marker types for different WBGT methods
-  methods     = ['liljegren', 'dimiceli', 'bernard']                            # Set names for different WBGT methods
-  #methods     = ['liljegren', 'dimiceli']                            # Set names for different WBGT methods
   variables   = ['Tg', 'Tpsy', 'Tnwb', 'Twbg']
   titles      = ['Globe Temperature', 'Psychrometric Wet Bulb', 'Natural Wet Bulb', 'Wet Bulb Globe Temperature']
   ylabels     = ['Globe Temperature (C)', 'Psychrometric WB', 'Natural WB', 'WBGT (C)', 'WBGT (C)']
   xlabels     = ['Globe Temperature (C)', 'Psychrometric WB', 'Psychrometric WB', 'WBGT (C)', 'WBGT (C; Bernard)']
 
-  nCol        = 5
   nVars       = len(variables)                                                  # Number of variables being plotted
   fig, axes   = plt.subplots(2, 2, figsize=(6,5))                                          # Create figure and subplots
   xyrange     = [ [float('inf'), -float('inf')] ] * nVars                       # Initialize ranges from each of the subplots
@@ -147,21 +150,26 @@ def main( *args ):
   clines = [ Line2D([0], [0], color=cmap(c/nBins), alpha=ALPHA, lw=4) for c in range(nBins)]       # Append line for the station in the legend
 
   fig.legend( flip(clines, nCol), flip(bins, nCol),
-         ncol = nCol, 
-         loc  = [0.05, 0.02]) 
+         ncol     = nCol, 
+         loc      = [0.02, 0.02],  
+         fontsize = 'x-small'
+  )
 
   fig.legend( 
     [Line2D([0], [0], marker=m, markersize=5, linewidth=0.0) for m in markers], 
     [m.title() for m in methods], 
-    loc = [0.8, 0.02],
+    loc      = [0.85, 0.02],
   ) 
 
-  plt.subplots_adjust(left=0.1,
-                    bottom=0.4, 
-                    right=0.98, 
-                    top=0.9, 
-                    wspace=0.18, 
-                    hspace=0.6) 
+  plt.subplots_adjust(
+    left = 0.1,
+    bottom = 0.25,
+    right  = 0.98, 
+    top    = 0.95, 
+    wspace = 0.05, 
+    hspace = 0.45
+  )
+   
 
   fig.savefig( '/Users/kwodzicki/Documents/NCICS/NOAA_Dello_113020_SE-HVI-WBGT/Figures/WBGT_by_rh_method.png',
     dpi = 300 ) 
--- seus_hvi_wbgt/validation/wbgt_by_wind_method.py
+++ seus_hvi_wbgt/validation/wbgt_by_wind_method.py
@@ -18,23 +18,26 @@ from .utils import *
 matplotlib.rcParams.update({'font.size': 8})
 ALPHA = 0.5
 
-def main( *args ):
+def main( *args, methods = None, nCol = 7, **kwargs):
   if len( args ) != 2:
     data, meta = c.download()
   else:
     data, meta = args
 
-  markers     = ['o', 'v', '^']                                                 # Set marker types for different WBGT methods
-  methods     = ['liljegren', 'dimiceli', 'bernard']                            # Set names for different WBGT methods
-  #methods     = ['liljegren', 'dimiceli']                            # Set names for different WBGT methods
-  variables   = ['Tg', 'WBGT']
-  titles      = ['Globe Temperature', 'Wet Bulb Globe Temperature']
-  ylabels     = ['Globe Temperature (C)', 'WBGT (C)', 'WBGT (C)']
-  xlabels     = ['Globe Temperature (C)', 'WBGT (C)', 'WBGT (C; Bernard)']
+  if methods is None:
+    methods     = ['liljegren', 'dimiceli', 'bernard']                            # Set names for different WBGT methods
 
-  nCol        = 5
+  if len(methods) > 3:
+    raise Exception( 'Can only test three (3) methods at a time!' )
+
+  markers     = ['o', 'v', '^']                                                 # Set marker types for different WBGT methods
+  variables   = ['Tg', 'Tpsy', 'Tnwb', 'Twbg']
+  titles      = ['Globe Temperature', 'Psychrometric Wet Bulb', 'Natural Wet Bulb', 'Wet Bulb Globe Temperature']
+  ylabels     = ['Globe Temperature (C)', 'Psychrometric WB', 'Natural WB', 'WBGT (C)', 'WBGT (C)']
+  xlabels     = ['Globe Temperature (C)', 'Psychrometric WB', 'Psychrometric WB', 'WBGT (C)', 'WBGT (C; Bernard)']
+
   nVars       = len(variables)                                                  # Number of variables being plotted
-  fig, axes   = plt.subplots(1, nVars, figsize=(6,5))                                          # Create figure and subplots
+  fig, axes   = plt.subplots(2, 2, figsize=(6,5))                                          # Create figure and subplots
   xyrange     = [ [float('inf'), -float('inf')] ] * nVars                       # Initialize ranges from each of the subplots
 
   cmap        = cm.get_cmap( 'jet' )                                            # Initialize colormap for station names
@@ -67,10 +70,14 @@ def main( *args ):
     speed    = addUnits( df, 'windspeed2m', dt )
 
     Tglobe   = addUnits( df, 'blackglobetemp2m', dt )
+    Tpsy     = addUnits( df, 'wetbulbtemp2m',    dt )
+    Tnwb     = addUnits( df, 'wetbulbtemp2m',    dt )
     Twbgt    = addUnits( df, 'wbgt2m',           dt )
 
     Tglobe   = Tglobe.to( 'degree_Celsius' ).magnitude                          # Globe temperature as measured at station
-    Twbgt    =  Twbgt.to( 'degree_Celsius' ).magnitude                          # Wet bulb globe as measured at station
+    Tpsy     = Tpsy.to(   'degree_Celsius' ).magnitude
+    Tnwb     = Tnwb.to(   'degree_Celsius' ).magnitude
+    Twbgt    = Twbgt.to(  'degree_Celsius' ).magnitude                          # Wet bulb globe as measured at station
 
     metaLoc  = meta[ meta['Location ID'] == location ] 
     lon      = metaLoc['Longitude [degrees East]' ].values.astype( numpy.float32 )
@@ -93,7 +100,10 @@ def main( *args ):
       )
 
       #for vidx, (xx, yy) in enumerate( zip( [Tglobe, Twbgt, TwbgtB], [res['Tg'], res['Twbg'], res['Twbg']] ) ):
-      for vidx, (var, xx, yy) in enumerate( zip( variables, [Tglobe, Twbgt], [res['Tg'], res['Twbg']] ) ):
+      for vidx, (var, xx) in enumerate( zip( variables, [Tglobe, Tpsy, Tnwb, Twbgt] ) ):
+        yy = res.get( var, None )
+        if yy is None: continue
+
         xyrange[vidx] = [min( [numpy.nanmin(xx), numpy.nanmax(yy), xyrange[vidx][0]] ), 
                          max( [numpy.nanmin(xx), numpy.nanmax(yy), xyrange[vidx][1]] ) ]
 
@@ -104,7 +114,7 @@ def main( *args ):
         RMSE_data[key]['y'].append( yy )
  
         colors = numpy.digitize( speed.m, bins ) / nBins 
-        axes[vidx].scatter( xx, yy, 
+        axes[vidx//2][vidx%2].scatter( xx, yy, 
           marker = markers[midx], 
           c      = cmap( colors ), 
           label  = location, 
@@ -112,41 +122,51 @@ def main( *args ):
 
   rmse = {}
   for key, vals in RMSE_data.items():
-    xx   = numpy.concatenate( vals['x'] )
-    yy   = numpy.concatenate( vals['y'] )
+    try:
+      xx   = numpy.concatenate( vals['x'] )
+      yy   = numpy.concatenate( vals['y'] )
+    except:
+      continue
 
     N    = numpy.sum( numpy.isfinite(xx) & numpy.isfinite(yy) )
     rmse =  numpy.sqrt( numpy.nansum( (xx-yy)**2 ) / N )
     print( f'{key} - {rmse}' )
 
   xyrange = numpy.asarray( xyrange ) + [-2, 2]
-  for i, ax in enumerate(axes):
-    ax.set_aspect( 'equal' ) 
-    ax.set_xlabel( xlabels[i] )
-    ax.set_ylabel( ylabels[i] )
-    ax.set_xlim( xyrange[i] )
-    ax.set_ylim( xyrange[i] )
-    ax.set_title( titles[i] )
-    ax.plot( xyrange[i], xyrange[i], '-k' ) 
+  ii = 0
+  for i, axi in enumerate(axes):
+    for j, ax in enumerate( axi ):
+      ax.set_aspect( 'equal' )
+      ax.set_xlabel( xlabels[ii] )
+      ax.set_ylabel( ylabels[ii] )
+      ax.set_xlim( xyrange[ii] )
+      ax.set_ylim( xyrange[ii] )
+      ax.set_title( titles[ii] )
+      ax.plot( xyrange[ii], xyrange[ii], '-k' )
+      ii += 1
 
   clines = [ Line2D([0], [0], color=cmap(c/nBins), alpha=ALPHA, lw=4) for c in range(nBins)]       # Append line for the station in the legend
 
   fig.legend( flip(clines, nCol), flip(bins, nCol),
-         ncol = nCol, 
-         loc  = [0.05, 0.02]) 
+         ncol     = nCol, 
+         loc      = [0.02, 0.02],
+         fontsize = 'x-small'
+  ) 
 
   fig.legend( 
     [Line2D([0], [0], marker=m, markersize=5, linewidth=0.0) for m in markers], 
     [m.title() for m in methods], 
-    loc = [0.8, 0.02],
+    loc = [0.85, 0.02],
   ) 
 
-  plt.subplots_adjust(left=0.1,
-                    bottom=0.4, 
-                    right=0.98, 
-                    top=0.9, 
-                    wspace=0.18, 
-                    hspace=0.18) 
-
+  plt.subplots_adjust(
+    left   = 0.1,
+    bottom = 0.25,
+    right  = 0.98, 
+    top    = 0.95, 
+    wspace = 0.05, 
+    hspace = 0.45
+  ) 
+ 
   fig.savefig( '/Users/kwodzicki/Documents/NCICS/NOAA_Dello_113020_SE-HVI-WBGT/Figures/WBGT_by_winds_method.png',
     dpi = 300 ) 
