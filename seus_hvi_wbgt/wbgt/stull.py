ba6f33695a25c6a9e3d0b759e28b908ad624c79c
Kyle R. Wodzicki <kwodzicki@cicsnc.org>
2022-12-19 16:45:54 -0500

    Adds Iribarne and Godson (1981) wet bulb
    The method from computing wet bulb temperature from ambient
    (dry-bulb) temperature, dewpoint tempeature, and atmospheric
    pressure from Iribarne and Godson (1981) has been added to the
    package. Implemented in cython for speed.

---
+++ seus_hvi_wbgt/wbgt/stull.py
@@ -0,0 +1,24 @@
+from numpy import arctan
+from .utils import relative_humidity
+
+def psychrometricWetBulb( Ta, RH ):
+  """
+  Wet bulb temperature from Stull method
+
+  This formula for wet bulb temperature appears in:
+    Stull, R. (2011). Wet-Bulb Temperature from Relative Humidity and 
+      Air Temperature, Journal of Applied Meteorology and Climatology, 
+      50(11), 2267-2269. Retrieved Jul 20, 2022, from 
+      https://journals.ametsoc.org/view/journals/apme/50/11/jamc-d-11-0143.1.xml
+
+  Inputs:
+    Ta (ndarray) : Ambient (dry bulb) temperature (degree C)
+    Td (ndarray) : Dew point temperature (degree C)
+
+  """
+
+  RH = relative_humidity( Ta, Td )
+  return Ta*arctan( 0.151977*(RH + 8.313659)**(1.0/2.0) ) +\
+         arctan( Ta + RH ) - arctan( RH - 1.676331 ) +\
+         0.00391838*RH**(3.0/2.0)*arctan( 0.023101*RH ) -\
+         4.686035
