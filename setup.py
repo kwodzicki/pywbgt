823306a4f1994b4dd4f82ee5e8660e965267840b
Kyle R. Wodzicki <kwodzicki@cicsnc.org>
2023-07-20 12:47:39 -0400

    Adds modified setup.py for pyproject install

--- setup.py
+++ setup.py
@@ -1,92 +1,56 @@
 import os
-from setuptools import setup, convert_path, find_packages, Extension
+from setuptools import setup, find_packages, Extension
 
-NAME  = 'seus_hvi_wbgt'
-DESC  = 'Package for SouthEast US Heat Vulnerability Index using Wet Bulb Globe Temperature.'
-AUTH  = 'Kyle R. Wodzicki'
-EMAIL = 'kwodzicki@cicsnc.org'
-URL   = ''
+from Cython.Build import cythonize
+import numpy
 
-SETUP_REQUIRES   = [
-    'cython',
-    'numpy',
-]
-
-INSTALL_REQUIRES = [
-    *SETUP_REQUIRES,
-    'scipy',
-    'pandas',
-    'pyarrow',
-    'metpy',
-    'pvlib',
-    'matplotlib',
-]
-
-# Get version number from version file
-main_ns  = {}
-ver_path = convert_path( os.path.join(NAME, 'version.py') )
-with open(ver_path) as ver_file:
-  exec(ver_file.read(), main_ns)
-
-# Set up some keyword arguments for compiling external extensions
-# (C/Cython code)
 EXTS_KWARGS = dict( 
     extra_compile_args = ['-fopenmp'],
     extra_link_args    = ['-fopenmp'],
 )
 
+NAME = 'seus_hvi_wbgt'
+
 # Define external extensions that must be compiled (C/Cython code)
 EXTS = [
-    Extension( 
-        f'{NAME}.wbgt.liljegren',
-        sources            = [
-            os.path.join(NAME, 'wbgt', 'liljegren.pyx'),
-            os.path.join(NAME, 'wbgt', 'src', 'spa_c.c'),
-        ],
-        include_dirs = [os.path.join(NAME, 'wbgt', 'src')],
-        depends      = ['spa_c.h'],
-        **EXTS_KWARGS,
-    ),
+#    Extension( 
+#        f'{NAME}.wbgt.liljegren',
+#        sources            = [
+#            os.path.join('src', NAME, 'wbgt', 'liljegren.pyx'),
+#            os.path.join('src', NAME, 'wbgt', 'src', 'spa_c.c'),
+#        ],
+#        include_dirs = [os.path.join('src', NAME, 'wbgt', 'src')],
+#        depends      = ['spa_c.h'],
+#        **EXTS_KWARGS,
+#    ),
     Extension( 
         f'{NAME}.wbgt.spa',
         sources      = [
-            os.path.join(NAME, 'wbgt', 'spa.pyx'),
-            os.path.join(NAME, 'wbgt', 'src', 'spa_c.c'),
+            os.path.join('src', NAME, 'wbgt', 'spa.pyx'),
+            os.path.join('src', NAME, 'wbgt', 'src', 'spa_c.c'),
         ],
-        include_dirs = [os.path.join(NAME, 'wbgt', 'src')],
+        include_dirs = [os.path.join('src', NAME, 'wbgt', 'src')],
         depends      = ['spa_c.h'],
         **EXTS_KWARGS,
     ),
-    Extension( 
-        f'{NAME}.wbgt.bernard',
-        sources      = [os.path.join(NAME, 'wbgt', 'bernard.pyx')],
-        **EXTS_KWARGS,
-    ),
-    Extension( 
-        f'{NAME}.wbgt.psychrometric_wetbulb',
-        sources      = [os.path.join(NAME, 'wbgt', 'psychrometric_wetbulb.pyx')],
-        **EXTS_KWARGS,
-    ),
+#    Extension( 
+#        f'{NAME}.wbgt.bernard',
+#        sources      = [os.path.join('src', NAME, 'wbgt', 'bernard.pyx')],
+#        **EXTS_KWARGS,
+#    ),
+#    Extension( 
+#        f'{NAME}.wbgt.psychrometric_wetbulb',
+#        sources      = [os.path.join('src', NAME, 'wbgt', 'psychrometric_wetbulb.pyx')],
+#        **EXTS_KWARGS,
+#    ),
 ]
 
 # Actually run the install
-if __name__ == "__main__":
-    from Cython.Build import cythonize
-    import numpy
-
-    setup(
-        name                 = NAME,
-        description          = DESC,
-        url                  = URL,
-        author               = AUTH,
-        author_email         = EMAIL,
-        version              = main_ns['__version__'],
-        packages             = [NAME],
-        setup_requires       = SETUP_REQUIRES,
-        install_requires     = INSTALL_REQUIRES,
-        include_package_data = True,
-        ext_modules          = cythonize( EXTS ),
-        include_dirs         = [numpy.get_include()],
-        scripts              = ['bin/wbgt_gui'],
-        zip_safe             = False,
-    )
+setup(
+    name                 = NAME,
+#    packages             = find_packages(where="src"),
+#    package_dir          = {"" : "src"},
+#    include_package_data = True,
+    ext_modules          = cythonize( EXTS ),
+    include_dirs         = [numpy.get_include()],
+)
