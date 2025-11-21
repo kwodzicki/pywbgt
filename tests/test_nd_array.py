754a85f5bfcce035af0075c879124d61f6438262
Kyle R. Wodzicki <kwodzicki@cicsnc.org>
2025-11-21 12:54:26 -0500

    Adds unit test for multidim/xarray
    Adds new unit test that builds an Xarray Dataset of dummy data
    to run through the WBGT algorithms to check for consistent
    results.
    
    The Dataset is multidimensional. The dimensions are stacked and
    units quantified for the reference run, with each variable
    pulled from the stacked/quantified Dataset as a numpy/Quantity.
    This is the reference data, with other unit tests ensuring that
    computations are correctly.
    
    The full, unmodified Dataset is then fed into the various
    algorithms. Results from the Dataset input and the aforementioned
    numpy/Quantity input are compared for equality.

---
+++ tests/test_nd_array.py
@@ -0,0 +1,104 @@
+import unittest
+
+import pandas
+import numpy as np
+import xarray as xr
+
+import pywbgt
+
+
+class TestNDArray(unittest.TestCase):
+    """
+    Test of multidimension/xarray support
+
+    """
+
+    def setUp(self):
+
+        dims = ('t', 'y', 'x')
+        dates = pandas.date_range(
+            start='2000-06-01T12',
+            end='2000-06-02T12',
+            freq='1D',
+        )
+        lats = np.asarray((33, 43, 59))
+        lons = np.asarray((-84, 22, 50, 120))
+
+        shape = (len(dates), len(lats), len(lons))
+
+        self.data = xr.Dataset(
+            data_vars=dict(
+                solar=(
+                    dims,
+                    np.random.random(shape) * 1000,
+                    {'units': 'watt/meter**2'},
+                ),
+                pres=(
+                    dims,
+                    np.random.random(shape) * 40 + 985,
+                    {'units': 'hPa'},
+                ),
+                temp_air=(
+                    dims,
+                    np.random.random(shape) * 10 + 25,
+                    {'units': 'degree_Celsius'},
+                ),
+                temp_dew=(
+                    dims,
+                    np.random.random(shape) * 10 + 15,
+                    {'units': 'degree_Celsius'},
+                ),
+                speed=(
+                    dims,
+                    np.random.random(shape) * 4 + 1,
+                    {'units': 'mile/hour'},
+                ),
+            ),
+            coords=dict(
+                t=(('t'), dates, {'axis': 'T'}),
+                y=(('y'), lats, {'axis': 'Y'}),
+                x=(('x'), lons, {'axis': 'X'}),
+            ),
+        )
+
+        self.new_dim = 'stacked'
+        self.ref_ds = self.data.metpy.quantify().stack({self.new_dim: dims})
+
+    def check_method(self, method):
+
+        ref = pywbgt.wbgt(
+            method,
+            self.ref_ds.t.data,
+            self.ref_ds.y.data,
+            self.ref_ds.x.data,
+            self.ref_ds.solar.data,
+            self.ref_ds.pres.data,
+            self.ref_ds.temp_air.data,
+            self.ref_ds.temp_dew.data,
+            self.ref_ds.speed.data,
+        )
+
+        ref = self.ref_ds.assign(
+            {
+                key: (self.new_dim, val)
+                for key, val in ref.items()
+                if hasattr(val, 'shape')
+            }
+        ).unstack().metpy.dequantify()
+
+        res = pywbgt.wbgt(method, self.data).metpy.dequantify()
+
+        for var in res.data_vars:
+            np.testing.assert_equal(res[var].values, ref[var].values)
+
+    def test_liljegren(self):
+        self.check_method('liljegren')
+
+    def test_dimiceli(self):
+        self.check_method('dimiceli')
+
+    def test_dimiceli_nws(self):
+        self.check_method('dimiceli_nws')
+
+    def test_bernard(self):
+        self.check_method('bernard')
